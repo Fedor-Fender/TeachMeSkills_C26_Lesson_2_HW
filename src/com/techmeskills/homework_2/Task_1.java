d36897fd31a74228d93dea215eb39ceb927c16fd
Fedor-Fender <developerc26java@gmail.com>
2023-11-14 11:22:36 +0300

    First commit
    This application can execute calculation of value a via formula

---
+++ src/com/techmeskills/homework_2/Task_1.java
@@ -0,0 +1,18 @@
+package com.techmeskills.homework_2;
+/**
+ * Write an application that'll calculate and display
+ * value of using formula:
+ */
+public class Task_1 {
+    public static void main(String[] args) {
+
+        //Input initial values
+        double b = 12;
+        double c = 8.7;
+        double a;
+        a =  (4 * (b + c - 1)) / 2;
+
+        //Output value
+        System.out.println("Output value of = "+a);
+    }
+}
